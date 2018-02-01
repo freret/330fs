af66b9dd1a10d9d770e1bae256e9a3ce5dbb2c81
Jake Nathanson <jake.nathanson@me.com>
2018-01-31 22:23:39 -0600

    stageed

--- Upload.php
+++ Upload.php
@@ -28,40 +28,29 @@
 <div id="testing">
 </div>
 
-
+<form enctype="multipart/form-data" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST">
+<div id=upload>
 <div id="tableview">
   <table style="width:100%">
     <tr>
-    <form enctype="multipart/form-data" action="uploader.php" method="POST">
       <th>Upload Buddy </th>
-
     </tr>
-
     <tr>
       <td>
-        <form enctype="multipart/form-data" action="uploader.php" method="POST">
         	<p>
         		<input type="hidden" name="MAX_FILE_SIZE" value="20000000" />
         		<label for="uploadfile_input">1. Choose a file to upload:</label> <input name="uploadedfile" type="file" id="uploadfile_input" />
         	</p>
-        	<p>
-
-        	</p>
-
       </td>
     </tr>
-
-    </tr>
-
-    </tr>
   </table>
-  <input type="submit" value="Upload File" id="upload" />
-  </form>
+</div>
+<input type="submit" value="Upload File" id="upload" />
+</form>
 </div>
 
 
 <?php
-session_start();
 
 // Get the filename and make sure it is valid
 $filename = basename($_FILES['uploadedfile']['name']);
