01ff21a9a1dc797665e3942128e83bffe368ad0f
Jake Nathanson <jake.nathanson@me.com>
2018-01-31 21:01:35 -0600

    download.php

--- 330FileServer.php
+++ 330FileServer.php
@@ -48,50 +48,20 @@
       <th>Size</th>
       <th>Actions</th>
     </tr>
+
     <tr>
       <td>Type</td>
       <td>Name</td>
       <td>Size </td>
-      <td> <input name="<?php echo $row['id']; ?>" type="submit" value="Download">
+      <td> <form action="download.php" method="post">
+        <input type='submit' name='submit' value='Download' class='register'/>
+      </form>
         <input name="<?php echo $row['id']; ?>" type="submit" id="<?php echo $row['id']; ?>"  value="Delete">
 
       </td>
     </tr>
   </table>
 </div>
-
-<?php
-function download($filename){
-
-    session_start();
-
-    //$filename = $_GET['file'];
-
-    if( !preg_match('/^[\w_\.\-]+$/', $filename) ){
-	  echo "Invalid filename";
-	  exit;
-    }
-
-    $username = $_SESSION['username'];
-    if( !preg_match('/^[\w_\-]+$/', $username) ){
-      	  echo "Invalid username";
-	      exit;
-}
-
-$full_path = sprintf("/home/rfreret/Module2/%s/%s", $username, $filename);
-
-// Now we need to get the MIME type (e.g., image/jpeg).  PHP provides a neat little interface to do this called finfo.
-$finfo = new finfo(FILEINFO_MIME_TYPE);
-$mime = $finfo->file($full_path);
-
-// Finally, set the Content-Type header to the MIME type of the file, and display the file.
-header("Content-Type: ".$mime);
-readfile($full_path);
-}
-?>
-
-
-
 
 
 </html>
---
+++ download.php
@@ -0,0 +1,29 @@
+<?php
+function download($filename){
+
+    session_start();
+
+    $filename = $_GET['file'];
+
+    if( !preg_match('/^[\w_\.\-]+$/', $filename) ){
+	  echo "Invalid filename";
+	  exit;
+    }
+
+    $username = $_SESSION['username'];
+    if( !preg_match('/^[\w_\-]+$/', $username) ){
+      	  echo "Invalid username";
+	      exit;
+}
+
+$full_path = sprintf("/home/rfreret/Module2/%s/%s", $username, $filename);
+
+// Now we need to get the MIME type (e.g., image/jpeg).  PHP provides a neat little interface to do this called finfo.
+$finfo = new finfo(FILEINFO_MIME_TYPE);
+$mime = $finfo->file($full_path);
+
+// Finally, set the Content-Type header to the MIME type of the file, and display the file.
+header("Content-Type: ".$mime);
+readfile($full_path);
+}
+?>
