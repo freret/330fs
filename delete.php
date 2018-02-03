9382261c316906bec933b354608ae1f0c74dafc5
Jake Nathanson <jake.nathanson@me.com>
2018-02-03 16:49:45 -0600

    jake

--- delete.php
+++ delete.php
@@ -22,8 +22,6 @@ if( !preg_match('/^[\w_\-]+$/', $username) ){
 $full_path = sprintf("/home/rfreret/Module2/%s/%s", $username, $filename);
 
 // Now we need to get the MIME type (e.g., image/jpeg).  PHP provides a neat little interface to do this called finfo.
-$finfo = new finfo(FILEINFO_MIME_TYPE);
-$mime = $finfo->file($full_path);
 
 unlink($full_path);
 header('Location: 330FileServer');
