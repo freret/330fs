b4b8840ba9c9115f0a45dd3a1cb6e2aa8bb366e4
Jake Nathanson <jake.nathanson@me.com>
2018-02-02 21:14:46 -0600

    redirect

--- delete.php
+++ delete.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 $filename = $_POST['filename'];
 
@@ -18,11 +19,13 @@ if( !preg_match('/^[\w_\-]+$/', $username) ){
 	exit;
 }
 
+$full_path = sprintf("/home/rfreret/Module2/%s/%s", $username, $filename);
 
+// Now we need to get the MIME type (e.g., image/jpeg).  PHP provides a neat little interface to do this called finfo.
+$finfo = new finfo(FILEINFO_MIME_TYPE);
+$mime = $finfo->file($full_path);
 
-
-
-
-
+delete($full_path);
+header('Location: 330FileServer');
 
 ?>
--- download.php
+++ download.php
@@ -21,6 +21,14 @@ if( !preg_match('/^[\w_\-]+$/', $username) ){
 
 $full_path = sprintf("/home/rfreret/Module2/%s/%s", $username, $filename);
 
-delete($file_path);
+// Now we need to get the MIME type (e.g., image/jpeg).  PHP provides a neat little interface to do this called finfo.
+$finfo = new finfo(FILEINFO_MIME_TYPE);
+$mime = $finfo->file($full_path);
+
+// Finally, set the Content-Type header to the MIME type of the file, and display the file.
+header("Content-Type: ".$mime);
+//header("Content-Type: application/octet-stream");
+
+readfile($full_path);
 
 ?>
